2c832d1ed43a5bbd1359e4411ae1eeb679db1328
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-04-08 00:19:49 +0600

    jk

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/upload", express.static(__dirname + "/upload/"))
 
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://127.0.0.1:3000",
     credentials: true
 }))
 
