d299b2224c2495c751b3903a6c5acb63eb954d71
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-04-08 00:59:45 +0600

    jk

--- app.js
+++ app.js
@@ -109,18 +109,18 @@ app.post("/login", async (req, res) => {
 
 // for profile getting the token from cookie and get the value of the token using jwt.varify() and the find the main user using that token information and send the information of the user
 app.get("/profile", async (req, res) => {
-    // let { token } = req.cookies
-    // if (token) {
-    //     jwt.verify(token, process.env.jwt_secret, {}, async (err, user) => {
-    //         let findUser= await usermodel.findById(user.id)
-    //         res.json(findUser)
-    //     })
-    // }
-    // else {
-    //     res.json(null);
-    // }
+    let { token } = req.cookies
+    if (token) {
+        jwt.verify(token, process.env.jwt_secret, {}, async (err, user) => {
+            let findUser= await usermodel.findById(user.id)
+            res.json(findUser)
+        })
+    }
+    else {
+        res.json(null);
+    }
  
-    res.json(token)
+    // res.json(token)
 })
 
 
