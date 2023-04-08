23c2393989b6e0076d84f6abfd79258d1bb772b5
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-04-08 21:37:57 +0600

    jk

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.post("/login", async (req, res) => {
                     id: user._id
                 }, process.env.jwt_secret,{expiresIn:"2d"},(err,token)=>{
                     if(err) throw err;
-                    res.cookie("token", token,{sameSite:"none"}).json(user)
+                    res.cookie("token", token,{secure:true,sameSite:"none", }).json(user)
                 } )
 
             }
